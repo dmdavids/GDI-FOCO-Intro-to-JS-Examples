2919dcbd67d9d13c1cac79ffca2432badf7372e5
Daun Davids <dmdavids@users.noreply.github.com>
2015-01-09 14:37:16 -0700

    Update javascript.js

--- Day 1/1c/javascript.js
+++ Day 1/1c/javascript.js
@@ -1,6 +1,6 @@
-var age = 51;
+var age = 29;
 var max = 101;
 var avg = 1;
 var amount = (max-age) * 365 * avg;
-var answer = "You will need "+ amount + " to last you untilran old age of  " + max;
+var answer = "You will need "+ amount + " to last you until an old age of  " + max;
 alert(answer);
